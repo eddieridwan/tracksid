de259d447492c332695ba65f3681dcd3a3c6502f
Eddie Ridwan <eddie.ridwan@amc.org.au>
2017-01-18 00:07:57 +1300

    Ubah nama berkas controller dan model mulai dengan huruf besar

--- application/controllers/track.php
+++ application/controllers/track.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class track extends CI_Controller {
+class Track extends CI_Controller {
 
   public function index()
   {
--- application/models/desa_model.php
+++ application/models/desa_model.php
@@ -1,6 +1,6 @@
 <?php
 
-class desa_model extends CI_Model{
+class Desa_model extends CI_Model{
 
   public function __construct(){
     parent::__construct();
