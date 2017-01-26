1581fff1f5515ab46c2f21cb0cc1f9be75eb4a62
Eddie Ridwan <eddie.ridwan@amc.org.au>
2017-01-26 20:12:02 +1300

    Tulis data setiap akses ke tabel akses

--- application/controllers/track.php
+++ application/controllers/track.php
@@ -3,17 +3,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Track extends CI_Controller {
 
+  function __construct(){
+    parent::__construct();
+    session_start();
+  }
+
   public function index()
   {
     $this->load->view('welcome_message');
   }
 
   public function desa(){
+    $_SESSION['seq'] = 1;
     $this->load->model('desa_model');
     echo "<pre>";
     print_r($_POST);
     echo "</pre>";
     $result = $this->desa_model->insert();
-    echo $result;
+    echo "Result: ".$result;
   }
 }
--- application/models/desa_model.php
+++ application/models/desa_model.php
@@ -5,11 +5,19 @@ class Desa_model extends CI_Model{
   public function __construct(){
     parent::__construct();
     $this->load->database();
+    $this->load->library('user_agent');
   }
 
   public function insert(){
     $data = $_POST;
     $data['tgl_ubah'] = date('Y-m-d H:i:s',time());
+    $version = $data['version'];
+    unset($data['version']);
+
+    // Masalah dengan auto_increment meloncat. Paksa supaya berurutan.
+    // https://ubuntuforums.org/showthread.php?t=2086550
+    $sql = "ALTER TABLE desa AUTO_INCREMENT = 1";
+    $this->db->query($sql);
 
     $sql = $this->db->set($data)->get_compiled_insert('desa');
     $sql .= "
@@ -27,7 +35,37 @@ class Desa_model extends CI_Model{
       ";
 
     $out = $this->db->query($sql);
-    return $out;
+    $desa_id = $this->db->insert_id();
+    // Kalau update, query lagi untuk mencari desa_id
+    if ($desa_id == 0) {
+      $this->db->where(array(
+        "nama_desa" => $data["nama_desa"],
+        "nama_kecamatan" => $data["nama_kecamatan"],
+        "nama_kabupaten" => $data["nama_kabupaten"],
+        "nama_provinsi" => $data["nama_provinsi"],
+        "ip_address" => $data["ip_address"]
+      ));
+      $query = $this->db->get("desa");
+      $desa = $query->row_array();
+      $desa_id = $desa['id'];
+    }
+
+    // Masalah dengan auto_increment meloncat. Paksa supaya berurutan.
+    // https://ubuntuforums.org/showthread.php?t=2086550
+    // PERHATIAN: insert ke tabel akses memasukkan dua baris duplikat, sehingga
+    // perlu dibuatkan index unik (perlu dicari sebabnya -- mungkin bug di mysql atau codeigniter)
+    $sql = "ALTER TABLE akses AUTO_INCREMENT = 1";
+    $this->db->query($sql);
+    $akses = [];
+    $akses['desa_id'] = $desa_id;
+    $akses['url_referrer'] = $this->agent->referrer();
+    $akses['client_ip'] = get_client_ip_server();
+    $akses['opensid_version'] = $version;
+    $akses['tgl'] = $data['tgl_ubah'];
+    $sql = $this->db->set($akses)->get_compiled_insert('akses');
+
+    $out2 = $this->db->query($sql);
+    return "desa: ".$out." akses: ".$out2;
   }
 
 }
