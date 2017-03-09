055aec03f1371438ae53d19e631d775c970d7982
Eddie Ridwan <eddie.ridwan@amc.org.au>
2017-03-09 01:15:42 +0000

    Pindahkan javascript ke awal file list_data.php

--- application/views/list_desa.php
+++ application/views/list_desa.php
@@ -3,6 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="<?php echo base_url()?>assets/js/bootstrap.min.js"></script>
 </head>
 <body>
 
@@ -67,9 +71,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 </div>
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="<?php echo base_url()?>assets/js/bootstrap.min.js"></script>
 </body>
 </html>
