4d915f5e9284997e55d7a364aa102d08f1050730
Eddie Ridwan <eddie.ridwan@amc.org.au>
2017-03-09 00:59:49 +0000

    ENVIRONMENT ditentukan oleh ip address server

--- index.php
+++ index.php
@@ -53,7 +53,9 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+	// define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+	// Use IP address to set environment
+	define('ENVIRONMENT', (strpos($_SERVER['REMOTE_ADDR'],'10.') === 0) ? 'development' : 'production');
 
 /*
  *---------------------------------------------------------------
